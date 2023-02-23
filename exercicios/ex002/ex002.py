8f49e828a78c6ad20a0041ad37c437dee181a9d3
Ivan Pereira <125929359+Ivan56-pereira@users.noreply.github.com>
2023-02-23 18:16:57 -0300

    Create ex002.py
    exercicio 2 feito com sucesso

---
+++ exercicios/ex002/ex002.py
@@ -0,0 +1,7 @@
+# ex002  crie um progama que leia o nome de uma pessoa e mostre uma mensagem de boas vindas
+
+# pengando o nome do usuario
+nome_usuario = str(input('Digite seu nome:'))
+
+# exibindo a mensagem de boas vindas
+print(f'prazer em conhece-lo {nome_usuario}')
