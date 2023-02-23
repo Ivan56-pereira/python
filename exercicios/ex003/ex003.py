fea6045b5ab7b0c484ff3eafcc317612216ace9b
Ivan Pereira <125929359+Ivan56-pereira@users.noreply.github.com>
2023-02-23 18:32:27 -0300

    exercicio feito com sucesso
    crie um programa que leia dois numeros e mostre a soma entre eles

---
+++ exercicios/ex003/ex003.py
@@ -0,0 +1,12 @@
+# crie um programa que leia dois numeros e mostre a soma entre eles.
+
+#pegando os dois valores do usuario
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite outro numero:'))
+
+# somado os valores 
+soma = num1 + num2
+
+# exibindo o resultado
+print(f'{num1} + { num2} = {soma}')
+
