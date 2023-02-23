f845624dcc0bc22e0b925a96798df766e1daee97
Ivan Pereira <125929359+Ivan56-pereira@users.noreply.github.com>
2023-02-23 19:43:55 -0300

    desafio 4 imconpleto
    dificil de cumprir o desafio

---
+++ exercicios/ex004.py
@@ -0,0 +1,9 @@
+#crie um programa que leia pelo teclado e mostre na tela o seu tipó primitivo e todas as imformacoes posiveis na tela
+
+# recebendo a informacao
+imformacao_usuario = int(input('Digite algo:'))
+
+# exibindo dados posiveis sobre a informacao dada 
+print('tem espaços :', a.space())
+print('e afabetico :', a.isalnum())
+
