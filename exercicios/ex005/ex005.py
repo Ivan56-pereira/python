f992734728c55a84ba9d1845f4ff306c63380781
Ivan Pereira <125929359+Ivan56-pereira@users.noreply.github.com>
2023-02-24 10:47:07 -0300

    criado com sucesso

---
+++ exercicios/ex005/ex005.py
@@ -0,0 +1,14 @@
+# o programa desta vez e, ler um numero inteiro  e mostrar na tela o seu antecessor e seu sucessor 
+
+# numero que o usuario vai digitar
+numero = int(input('Digite um numero:'))
+
+# somando e subitraindo os valores
+antecessor = numero - 1 
+sucessor = numero + 1 
+
+# mostrando o resultado
+print('Analizando o numero')
+print(F'{numero} seu antecessor e {antecessor} e seu sucessor e {sucessor}')
+
+
